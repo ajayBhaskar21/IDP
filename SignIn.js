8457eca986d2bca8a125bcb74c175b62a370bb6c
Broken <124747342+ajayBhaskar21@users.noreply.github.com>
2023-10-17 17:59:33 +0530

    Add files via upload

--- SignIn.js
+++ SignIn.js
@@ -2,28 +2,73 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
+import { database } from './firebase'; // Adjust the path as needed
+import { ref, get, child, } from 'firebase/database';
 
 const SignIn = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigation = useNavigation();
 
-  const handleSignIn = () => {
-    // Replace this with your actual authentication logic
-    if (username === 'yourUsername' && password === 'yourPassword') {
-      // Successful login, navigate to "Home" screen
-      navigation.navigate('Home');
+  const handleSignIn = async () => {
+
+    // Test  to fetch the data
+
+
+    // Reference to the "users" collection
+    const usersRef = ref(database, 'users');
+
+    // Fetch all user data from the "users" collection
+    
+      const usersSnapshot = await get(usersRef);
+
+      if (usersSnapshot.exists()) {
+        // Convert the user data to an array
+        const usersArray = [];
+        usersSnapshot.forEach((childSnapshot) => {
+          const user = {
+            id: childSnapshot.key, // The user's unique ID
+            ...childSnapshot.val(), // Other user data (e.g., username, password)
+          };
+          usersArray.push(childSnapshot.val().username);
+        });
+        console.log('userArr = ' + usersArray);
+      } else {
+        // Handle the case where the "users" collection is empty
+        console.log('No users found.');
+      }
+
+
+
+    /*
+    // Reference to the user in the Firebase Realtime Database
+    const userRef = ref(database, 'users');
+
+    // Check if the user exists in the database
+    const userSnapshot = await get(child(userRef, username));
+    console.log('user snapshot = ' + userSnapshot);
+    console.log('user snapshot val = ' + userSnapshot.val());
+    if (userSnapshot.val() !== null) {
+      // Get the user's password from the database
+      const savedPasswordSnapshot = await get(child(userRef, username, 'password'));
+
+      if (savedPasswordSnapshot.val() !== null && password === savedPasswordSnapshot.val()) {
+        // Successful login, navigate to "Home" screen
+        navigation.navigate('Home');
+      } else {
+        // Failed login, show an error message
+        alert('Invalid username or password');
+      }
     } else {
-      // Failed login, show an error message
-      alert('Invalid username or password');
+      // User does not exist in the database
+      alert('User not found');
     }
+    */
+
   };
 
   return (
-    <LinearGradient
-      colors={['#3494E6', '#EC6EAD']}
-      style={styles.container}
-    >
+    <LinearGradient colors={['#3494E6', '#EC6EAD']} style={styles.container}>
       <Text style={styles.header}>Sign In</Text>
       <TextInput
         style={styles.input}
