5e03c993e178cdbccefccd09e3b75ebc79c10cc6
Broken <124747342+ajayBhaskar21@users.noreply.github.com>
2023-11-09 15:59:44 +0530

    check config from firebase console

--- firebase1.js
+++ firebase1.js
@@ -5,13 +5,7 @@ let firebaseApp = null;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBlVPqSK2dGMUX3Iz6lENmhpRzokwDNZg4",
-    authDomain: "idp-1-84f20.firebaseapp.com",
-    databaseURL: "https://idp-1-84f20-default-rtdb.firebaseio.com",
-    projectId: "idp-1-84f20",
-    storageBucket: "idp-1-84f20.appspot.com",
-    messagingSenderId: "377092112075",
-    appId: "1:377092112075:web:d6487bdc04b512efb8a54e"
+    
 };
 
 if (!firebaseApp) {
